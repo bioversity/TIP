8668c966bbc8a1ed0107f737636e0f8ff48092f4
aczepod <aczepod@gmail.com>
2012-09-14 14:54:22 +0200

    new template

--- web/js/arbor_site_map_setup.js
+++ web/js/arbor_site_map_setup.js
@@ -8,79 +8,29 @@ var colour = {
 var data = {
   nodes:{
     HOME:{
-      color:colour.root_page,
-      shape:'dot',
-      radius:39,
-      alpha:1,
-      label: 'HOMEPAGE',
-      link: '/'},
+      color:colour.root_page, shape:'dot', radius:39, alpha:1, label: 'HOMEPAGE', link: '/'},
     LR:{
-      color:colour.research_page, 
-      shape:'dot', 
-      radius:30,
-      alpha:1,
-      label: 'LANDRACE',
-      link: '/admin/browse-landrace'},
+      color:colour.research_page, shape:'dot', radius:30, alpha:1, label: 'LANDRACE', link: '/browse-landrace'},
     CWR:{
-      color:colour.research_page, 
-      shape:'dot',
-      radius:40,
-      alpha:1,
-      label: 'CROP WILD RELATIVES',
-      link: '/admin/browse-cwr'},
+      color:colour.research_page, shape:'dot', radius:40, alpha:1, label: "CROP WILD RELATIVES", link: '/browse-cwr'},
    TRAIT: {
-      color:colour.research_page,               
-      shape:'dot',
-      radius:35,
-      alpha:1,
-      label: 'TRAIT',
-      link: '/admin/browse-trait'},
+      color:colour.research_page, shape:'dot', radius:35, alpha:1, label: 'TRAIT', link: '/browse-trait'},
    ABOUT: {
-      color:colour.static_page,               
-      shape:'dot',
-      radius:35,
-      alpha:1,
-      label: 'ABOUT',
-      link: '/about'},
+      color:colour.static_page, shape:'dot', radius:35, alpha:1, label: 'ABOUT', link: '/about'},
    CONTACT: {
-      color:colour.static_page,               
-      shape:'dot',
-      radius:35,
-      alpha:1,
-      label: 'CONTACT',
-      link: '/contact'},
+      color:colour.static_page, shape:'dot', radius:35, alpha:1, label: 'CONTACT', link: '/contact'},
    DATABASE: {
-      color:colour.static_page,               
-      shape:'dot',
-      radius:35,
-      alpha:1,
-      label: 'DATABASE',
-      link: '/database'},
+      color:colour.static_page, shape:'dot', radius:35, alpha:1, label: 'DATABASE', link: '/database'},
    DATABASE1: {
-      color:colour.static_page,
-      radius:35,
-      alpha:0,
-      label: 'DATASETS'},
+      color:colour.static_page, radius:35, alpha:0, label: 'DATASETS'},
    DATABASE2: {
-      color:colour.static_page,
-      radius:35,
-      alpha:0,
-      label: 'DATA SEARCH'},
+      color:colour.static_page, radius:35, alpha:0, label: 'DATA SEARCH'},
    DATABASE3: {
-      color:colour.static_page,
-      radius:35,
-      alpha:0,
-      label: 'DOWNLOAD DATA'},
+      color:colour.static_page, radius:35, alpha:0, label: 'DOWNLOAD DATA'},
    DATABASE4: {
-      color:colour.static_page,
-      radius:35,
-      alpha:0,
-      label: 'REQUEST DATA'},
+      color:colour.static_page, radius:35, alpha:0, label: 'REQUEST DATA'},
    DATABASE5: {
-      color:colour.static_page,
-      radius:35,
-      alpha:0,
-      label: 'CONTRIBUTE DATA'},
+      color:colour.static_page, radius:35, alpha:0, label: 'CONTRIBUTE DATA'},
   },
   edges:{
     HOME:{
@@ -100,23 +50,27 @@ var data = {
     }
   }
 }
-    
-var sys = arbor.ParticleSystem()
-sys.parameters({stiffness:900, repulsion:2000, gravity:true, dt:0.015})
-sys.renderer = Renderer("#viewport");
-sys.graft(data); 
 
-$("#viewport").mousedown(function(e){
-  var pos = $(this).offset();
-  var p = arbor.Point(e.pageX-pos.left, e.pageY-pos.top);
-  selected = sys.nearest(p);
-  
-  $(this).mouseup(function(e){
-    if(selected.distance < 50){
-      var new_p= arbor.Point(e.pageX-pos.left, e.pageY-pos.top);
-      if(p.x == new_p.x && p.y == new_p.y)
-        if(selected.node.data.link)
-          window.location = selected.node.data.link;
-    }
-  });
+$(document).ready(function(){
+  $("#viewport").attr('width', $("#viewport").width());
 });
+  
+  var sys = arbor.ParticleSystem()
+  sys.parameters({stiffness:900, repulsion:2000, gravity:true, dt:0.015})
+  sys.renderer = Renderer("#viewport");
+  sys.graft(data); 
+
+  $("#viewport").mousedown(function(e){
+    var pos = $(this).offset();
+    var p = arbor.Point(e.pageX-pos.left, e.pageY-pos.top);
+    selected = sys.nearest(p);
+    
+    $(this).mouseup(function(e){
+      if(selected.distance < 50){
+        var new_p= arbor.Point(e.pageX-pos.left, e.pageY-pos.top);
+        if(p.x == new_p.x && p.y == new_p.y)
+          if(selected.node.data.link)
+            window.location = selected.node.data.link;
+      }
+    });
+  });
