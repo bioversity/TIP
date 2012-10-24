0696d3a1839e3f266ea32209b7d6e9d3940c1667
aczepod <aczepod@gmail.com>
2012-10-24 14:25:23 +0200

    empty folder

--- app/app.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once __DIR__.'/bootstrap.php';
-require_once __DIR__.'/routing.php';
-require_once __DIR__.'/session.php';
-require_once __DIR__.'/database.php';
-
-return $app;
--- app/dashboard.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-// display dashboard
-return $app['twig']->render('dashboard.twig', array('link_active' => 'dashboard'));
