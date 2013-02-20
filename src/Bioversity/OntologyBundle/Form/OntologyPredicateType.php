15231cb99e89d073c7a54421e449de1840e9c31f
aczepod <aczepod@gmail.com>
2013-02-20 10:15:01 +0100

    node creation form multisteps with same errors + predicate forms

---
+++ src/Bioversity/OntologyBundle/Form/OntologyPredicateType.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Bioversity\OntologyBundle\Form;
+
+use Bioversity\OntologyBundle\Form\OntologyBaseType;
+use Bioversity\ServerConnectionBundle\Repository\Tags;
+
+class OntologyPredicateType extends OntologyBaseType
+{   
+    var $internationlization= array(
+        //(int) Tags::kTAG_TERM,
+        Tags::kTAG_NAMESPACE,
+        Tags::kTAG_LID,
+        Tags::kTAG_LABEL,
+        Tags::kTAG_DEFINITION,
+        Tags::kTAG_SYNONYMS,
+        Tags::kTAG_CATEGORY
+    );
+    
+    public function getName()
+    {
+        return 'OntologyPredicate';
+    }
+}
