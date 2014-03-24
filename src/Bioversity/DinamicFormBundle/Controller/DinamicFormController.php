6b3c0edeab8549e543cd3ae3d0d96f9571269408
Milko Škofič <info@skofic.net>
2014-03-24 19:00:01 +0100

    Removed spaces from accordeon anchors: the anchor was the label of the term, which is not a good idea. In the future I must change it. Discovered another bug: urls do not work as term identifiers, must find a fix...

--- src/Bioversity/DinamicFormBundle/Controller/DinamicFormController.php
+++ src/Bioversity/DinamicFormBundle/Controller/DinamicFormController.php
@@ -17,42 +17,25 @@ class DinamicFormController extends Controller
 
 	    $formFields = array
 	    (
-			18378 => array
+		    18129 => array
 			(
-				18379 => array
-				(
-					18380, 18381, 18382, 18383, 18384, 18385, 18386,
-					18392, 18393, 18394, 18395, 18396, 18397, 18398,
-					18399, 18400
-				),
-				18401 => array
-				(
-					18402, 18403, 18404, 18405, 18406, 18407, 18408,
-					18409, 19410, 18411, 18412, 18413, 18414, 18415,
-					18416, 18417
-				)
+			    18130 => array( 522, 523 ),
+			    18133 => array( 524, 525, 526 ),
+			    18137 => array( 527, 528, 529, 530, 531, 532 ),
+			    18144 => array( 533, 534, 535, 536, 537, 538, 539, 540, 541 ),
+	//		    18154 => array( 452, 543, 544 ),
 			)
 	    );
 
- /*
-        $formFields=    Array(24575 => array(
-                                24576 => array(709),
-                                24578 => array(711,722),
-                                24601 => array(731),
-                                24598 => array(729, 730)
-                            )
-                        );
- */
-        
         //$formBase = $this->createForm(new BioversityDinamicFormType($formFields));
-        
+
         $dinamicForm= BioversityDinamicFormType::createFieldSet($formFields);
         foreach($dinamicForm as $groups=>$forms){
             foreach($forms as $key=>$form){
                 $dinamicForm[$groups][$key]= $this->createForm(new BioversityDinamicFormType($form))->createView();
             }
         }
-        
+
         return $this->render('BioversityDinamicFormBundle:DinamicForm:index.html.twig',
             array(
                 'form_base'         => $dinamicForm,
